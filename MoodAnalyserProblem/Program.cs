424c7bf0854ae85d092a87a3bb5c68e59d0fb31c
yash-agrwl <agarwalyash7012@gmail.com>
2022-04-03 20:29:42 +0530

    added empty project file with welcome message

---
+++ MoodAnalyserProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoodAnalyserProblem
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to the Mood Analyser Problem.\n");
+        }
+    }
+}
