837680ff1c9b3086aaecccdb6118386268373b5d
yash-agrwl <agarwalyash7012@gmail.com>
2022-04-03 21:00:32 +0530

    added UC1: Ability to analyse and respond Happy or Sad Mood

---
+++ MoodAnalyserProblem/MoodAnalyser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserProblem
+{
+    public class MoodAnalyser
+    {
+        public string AnalyseMood(string message)
+        {
+            if (message.Contains("Sad"))
+                return "SAD";
+            else
+                return "HAPPY";
+        }
+    }
+}
--- MoodAnalyserProblem/Program.cs
+++ MoodAnalyserProblem/Program.cs
@@ -7,6 +7,10 @@ namespace MoodAnalyserProblem
         static void Main()
         {
             Console.WriteLine("Welcome to the Mood Analyser Problem.\n");
+
+            MoodAnalyser mood = new();
+            Console.WriteLine(mood.AnalyseMood("I am in a Sad Mood"));
+            Console.WriteLine(mood.AnalyseMood("I am in a Happy Mood"));
         }
     }
 }
