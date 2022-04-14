29c698f8b8039855ba12c0f040ac54c31d6fb166
yash-agrwl <agarwalyash7012@gmail.com>
2022-04-14 22:04:30 +0530

    added TC2.1: Given Null Mood Should Return Happy

--- MoodAnalyserMSTest/TestMood.cs
+++ MoodAnalyserMSTest/TestMood.cs
@@ -4,7 +4,7 @@ using MoodAnalyserProblem;
 namespace MoodAnalyserMSTest
 {
     [TestClass]
-    public class UnitTest1
+    public class TestMood
     {
         /// <summary>
         /// TC 1.1: Given "I am in Sad Mood" message should return SAD.
@@ -28,11 +28,29 @@ namespace MoodAnalyserMSTest
         /// TC 1.2: Given "I am in Any Mood" message should return HAPPY.
         /// </summary>
         [TestMethod]
-        public void GivenAnyMoodShouldReturnHappy()
+        //Arrange
+        [DataRow("I am in Any Mood", "HAPPY")]
+        public void GivenAnyMoodShouldReturnHappy(string message, string expected)
         {
             //Arrange
-            string expected = "HAPPY";
-            string message = "I am in Any Mood";
+            MoodAnalyser mood = new(message);
+
+            //Act
+            string actual = mood.AnalyseMood();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// TC 2.1: Given null message should return HAPPY.
+        /// </summary>
+        [TestMethod]
+        //Arrange
+        [DataRow(null, "HAPPY")]
+        public void GivenNullMoodShouldReturnHappy(string message, string expected)
+        {
+            //Arrange
             MoodAnalyser mood = new(message);
 
             //Act
