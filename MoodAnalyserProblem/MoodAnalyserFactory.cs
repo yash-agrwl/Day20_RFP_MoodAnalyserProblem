526f4d6ebafc5d558869dca6aac8f5a29151856b
yash-agrwl <agarwalyash7012@gmail.com>
2022-05-03 10:42:37 +0530

    added UC5: Use Reflection to Create MoodAnalyser Object With Parameterized Constructor

--- MoodAnalyserProblem/MoodAnalyserFactory.cs
+++ MoodAnalyserProblem/MoodAnalyserFactory.cs
@@ -11,7 +11,7 @@ namespace MoodAnalyserProblem
     public class MoodAnalyserFactory
     {
         /// <summary>
-        /// UC 4: Method to create Object of MoodAnalyser class with Default Constructor using Reflection.
+        /// UC 4: Create Object of MoodAnalyser class with Default Constructor using Reflection.
         /// </summary>
         /// <param name="className"></param>
         /// <param name="constructorName"></param>
@@ -38,6 +38,38 @@ namespace MoodAnalyserProblem
             else
             {
                 throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_METHOD, "Constructor is Not Found");
+            }
+        }
+
+        /// <summary>
+        /// UC5: Create Object of MoodAnalyser class with Parameterized Constructor using Reflection.
+        /// </summary>
+        /// <param name="className"></param>
+        /// <param name="constructorName"></param>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        /// <exception cref="MoodAnalyserCustomException"></exception>
+        public static object CreateMoodAnalyserUsingParameterizedConstructor(string className, string constructorName, string message)
+        {
+            Type type = typeof(MoodAnalyser);
+            //Type type = Type.GetType("MoodAnalyserProblem.MoodAnalyser");
+
+            if (type.Name.Equals(className) || type.FullName.Equals(className))
+            {
+                if (type.Name.Equals(constructorName))
+                {
+                    ConstructorInfo ctor = type.GetConstructor(new[] { typeof(string) });
+                    object instance = ctor.Invoke(new object[] { message });
+                    return instance;
+                }
+                else
+                {
+                    throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_METHOD, "Constructor is Not Found");
+                }
+            }
+            else
+            {
+                throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_CLASS, "Class Not Found");
             }
         }
     }
